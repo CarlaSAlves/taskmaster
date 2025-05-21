0a029cd367f2971a833af0c256bb4c8ae4b92b1c
csoeiroa <carlanatalina.soeiroalves@altran.com>
2025-05-21 09:29:47 +0100

    Added image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Header.css';
+import todoLogo from '../../assets/to-do-list.png';
 
 const Header = () => {
   return (
     <header className='header'>
         <div className="container">
-            <img src="./assets/to-do-list.png" alt="logo" />
+            <img src={todoLogo} alt="logo" />
             <h1>Task Master</h1>
             <p>Manage your tasks efficiently</p>
         </div>
